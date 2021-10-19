f87cf821f428c66fae7c090bdb098683a4177ad1
Yunus <vokhstag@gmail.com>
2021-10-19 13:06:08 +0300

    Создал проект AnimationsHW
    Добавил проект для выполнения домашнего задания по анимации

---
+++ HomeWorks/AnimationsHW/AnimationsHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationsHW
+//
+//  Created by Yunus Abubakarov on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
